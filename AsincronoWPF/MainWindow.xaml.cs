791d07a4484680eba9d018cad88aae24a087c596
Lor3nzoF4bbri1 <lorenzo.fabbri01.stud@ispascalcomandini.it>
2021-03-27 17:55:43 +0100

    Step2: rimossi elementi non richiesti, aggiunte immagini

--- AsincronoWPF/MainWindow.xaml.cs
+++ AsincronoWPF/MainWindow.xaml.cs
@@ -22,32 +22,37 @@ namespace AsincronoWPF
     public partial class MainWindow : Window
     {
         Random r;
-        int sorteggiato;
+        int sorteggiato1;
+        int sorteggiato2;
+
+        readonly Uri uriDado1 = new Uri("dado-1.jpg", UriKind.Relative);
+        readonly Uri uriDado2 = new Uri("dado-2.jpg", UriKind.Relative);
+        readonly Uri uriDado3 = new Uri("dado-3.png", UriKind.Relative);
+        readonly Uri uriDado4 = new Uri("dado-4.png", UriKind.Relative);
+        readonly Uri uriDado5 = new Uri("dado-5.png", UriKind.Relative);
+        readonly Uri uriDado6 = new Uri("dado-6.png", UriKind.Relative);
+
+        ImageSource dado1;
+        ImageSource dado2;
+        ImageSource dado3;
+        ImageSource dado4;
+        ImageSource dado5;
+        ImageSource dado6;
 
         public MainWindow()
         {
             InitializeComponent();
 
+            dado1 = new BitmapImage(uriDado1);
+            dado2 = new BitmapImage(uriDado2);
+            dado3 = new BitmapImage(uriDado3);
+            dado4 = new BitmapImage(uriDado4);
+            dado5 = new BitmapImage(uriDado5);
+            dado6 = new BitmapImage(uriDado6);
+
             r = new Random();
 
             Sorteggio();
-        }
-
-        private void btn1_Click(object sender, RoutedEventArgs e)
-        {
-            ExecuteLongOp();
-            MessageBox.Show("Operazione sincrona completata");
-        }
-
-        private async void ExecuteLongOp()
-        {
-            await Task.Run(() =>
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    Thread.Sleep(1000);
-                }
-            });
         }
 
         private async void Sorteggio()
@@ -56,10 +61,11 @@ namespace AsincronoWPF
             {
                 for (;;)
                 {
-                    sorteggiato = r.Next(1, 1001);
+                    sorteggiato1 = r.Next(1, 7);
+                    sorteggiato2 = r.Next(1, 7);
                     this.Dispatcher.BeginInvoke(new Action(() =>
                     {
-                        lblRandom.Content = sorteggiato;
+                        imgDadoN1.Source = dado5;
                     }));
                     Thread.Sleep(100);
                 }
@@ -68,7 +74,7 @@ namespace AsincronoWPF
 
         private void btnSorteggia_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show(sorteggiato.ToString());
+            //MessageBox.Show(sorteggiato.ToString());
         }
     }
 }
